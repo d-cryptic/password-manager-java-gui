725ef12440d878c7a9ddd3f2e00b725f1ff326ac
Barun Debnath <barundebnath91@gmail.com>
2021-11-15 07:03:18 +0530

    Main method done

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+// Main method 
+
+public class Main {
+	public static void main(String[] args) {
+		// loading screen class  
+		new SplashScreen();
+		new PasswordManager();
+		try{
+			UIManager.setLookAndFeel("com.jtattoo.plaf.mint.MintLookAndFeel");
+		} catch (Exception e) {
+			ex.printStackTrace();
+		}
+	}	
+}
