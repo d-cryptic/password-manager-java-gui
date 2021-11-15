11e125bcf8f6d3964cdb2057322d5e7306976f37
Barun Debnath <barundebnath91@gmail.com>
2021-11-15 07:01:06 +0530

    SplashScreen done:

--- src/hashTableMap.java
+++ src/hashTableMap.java
@@ -24,3 +24,11 @@ public interface hashTableMap {
     int add_Acc(Object Account, Object passwd);
     Object remove_Acc(Object Account);
 }
+
+// HashMap and Hashtable store key and value pairs in a hash table. 
+// When using a Hashtable or HashMap, we specify an object that is used as a key and the value that you want to be linked to that key.
+//The key is then hashed, and the resulting hash code is used as the index at which the value is stored within the table
+
+//HashMap is non-synchronized. It is not thread-safe and can’t be shared between many threads without proper synchronization code whereas Hashtable is synchronized. It is thread-safe and can be shared with many threads.
+// HashMap allows one null key and multiple null values whereas Hashtable doesn’t allow any null key or value.
+//HashMap is generally preferred over HashTable if thread synchronization is not needed
